e1237ddb76f817747ad898cbf2d180f42f95b309
aekarach-jo <83202142+aekarach-jo@users.noreply.github.com>
2021-12-12 16:51:56 +0700

    Update call-api.service.ts

--- src/app/services/call-api.service.ts
+++ src/app/services/call-api.service.ts
@@ -15,56 +15,56 @@ export class CallApiService {
 
   // Room
   public getAllRoom(){
-    return this.http.get<room>(`${environment.apiUrl}Room/GetAllRoom`)
+    return this.http.get<room>(`${environment.https://localhost:5001/api/}Room/GetAllRoom`)
   }
 
   public getRoomById(roomId : string){
-    return this.http.get<room>(`${environment.apiUrl}Room/GetRoomById/${roomId}`)
+    return this.http.get<room>(`${environment.https://localhost:5001/api/}Room/GetRoomById/${roomId}`)
   }
 
   public createRoom(room : room ){
-    return this.http.post<room>(`${environment.apiUrl}Room/CreateRoom`, room)
+    return this.http.post<room>(`${environment.https://localhost:5001/api/}Room/CreateRoom`, room)
   }
   public editRoom(roomId : string , room : room){
-    return this.http.put<room>(`${environment.apiUrl}Room/EditRoom/${roomId}`,room)
+    return this.http.put<room>(`${environment.https://localhost:5001/api/}Room/EditRoom/${roomId}`,room)
   }
   public deleteRoom(roomId : string){
-    return this.http.get<room>(`${environment.apiUrl}Room/DeleteRoom/${roomId}`)
+    return this.http.get<room>(`${environment.https://localhost:5001/api/}Room/DeleteRoom/${roomId}`)
   }
 
   // User
 
   public getAllUser(){
-    return this.http.get<user>(`${environment.apiUrl}User/GetAllUser`)
+    return this.http.get<user>(`${environment.https://localhost:5001/api/}User/GetAllUser`)
   }
   public getUserById(userId : string){
-    return this.http.get<user>(`${environment.apiUrl}User/GetUserById/${userId}`)
+    return this.http.get<user>(`${environment.https://localhost:5001/api/}User/GetUserById/${userId}`)
   }
   public createUser(user : user ){
-    return this.http.post<user>(`${environment.apiUrl}User/CreateUser`, user)
+    return this.http.post<user>(`${environment.https://localhost:5001/api/}User/CreateUser`, user)
   }
   public editUser(userId : string , user : user){
-    return this.http.put<user>(`${environment.apiUrl}User/EditUser/${userId}`,user)
+    return this.http.put<user>(`${environment.https://localhost:5001/api/}User/EditUser/${userId}`,user)
   }
   public deleteUser(userId : string){
-    return this.http.get<user>(`${environment.apiUrl}User/DeleteUser/${userId}`)
+    return this.http.get<user>(`${environment.https://localhost:5001/api/}User/DeleteUser/${userId}`)
   }
   // Meter
 
   public getAllMeter(){
-    return this.http.get<meter>(`${environment.apiUrl}Meter/GetAllMeter`)
+    return this.http.get<meter>(`${environment.https://localhost:5001/api/}Meter/GetAllMeter`)
   }
   public getMeterById(meterId : string){
-    return this.http.get<meter>(`${environment.apiUrl}Meter/GetMeterById/${meterId}`)
+    return this.http.get<meter>(`${environment.https://localhost:5001/api/}Meter/GetMeterById/${meterId}`)
   }
   public createMeter(meter : meter ){
-    return this.http.post<meter>(`${environment.apiUrl}Meter/CreateMeter`, meter)
+    return this.http.post<meter>(`${environment.https://localhost:5001/api/}Meter/CreateMeter`, meter)
   }
   public editMeter(meterId : string , meter : meter){
-    return this.http.put<meter>(`${environment.apiUrl}Meter/EditMeter/${meterId}`,meter)
+    return this.http.put<meter>(`${environment.https://localhost:5001/api/}Meter/EditMeter/${meterId}`,meter)
   }
   public deleteMeter(meterId : string){
-    return this.http.get<meter>(`${environment.apiUrl}Meter/DeleteMeter/${meterId}`)
+    return this.http.get<meter>(`${environment.https://localhost:5001/api/}Meter/DeleteMeter/${meterId}`)
   }
 
 }
