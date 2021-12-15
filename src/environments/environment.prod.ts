e7741bab6a4cbd86e0b2811de32ae8101ac3a02c
aekarach-jo <aekarach.jomkhamsing@gmail.com>
2021-12-15 10:14:47 +0700

    add export excel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
   production: false,
   // apiUrl: "https://localhost:5001/api/"
-=======
-  production: true,
-  apiUrl: "https://localhost:5001/api/"
->>>>>>> aa2465c79386a6bd59801e3497894a23f5527cdf
 };
